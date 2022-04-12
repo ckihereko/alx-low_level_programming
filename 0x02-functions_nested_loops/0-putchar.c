9940d646d3827a4dca757b8e19d3f7cf6ad4fbb5
Chris <ckihereko@gmail.com>
2022-04-12 07:38:40 -0700

    Task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 
@@ -44,28 +43,3 @@ _putchar(n);
 return (0);
 
 }
-=======
-#include "main.h"
-
-
-/**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
-}
->>>>>>> 588dad8f2f5cbc6f5c1a1d167d0f373fd477a69f
