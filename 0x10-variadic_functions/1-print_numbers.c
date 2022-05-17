f85df3bf48d990837af50a8011b70bde0a31ea1c
ckihereko <102221386+ckihereko@users.noreply.github.com>
2022-05-17 03:49:01 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,35 +1,26 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_numbers - prints numbers
- * @separator: pointer to a char
- * @n: number of arguments passed
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
+ * Return: no return.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-		va_list num_print;
-		unsigned int i;
+	va_list valist;
+	unsigned int i;
 
-		va_start(num_print, n);
-		for (i = 0; i < n - 1; i++)
-		{
-			if (!separator)
-			{
-				printf("%d", va_arg(num_print, unsigned int));
-			}
-			if (n && separator)
-			{
-				if (i == 0)
-					printf("%d", va_arg(num_print, unsigned int));
-				else
-			printf("%d", va_arg(num_print, unsigned int));
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
 			printf("%s", separator);
-			}
-		}
-		if (i == n - 1)
-			printf("%d\n", va_arg(num_print, unsigned int));
-		va_end(num_print);
+	}
+
+	printf("\n");
+	va_end(valist);
 }
